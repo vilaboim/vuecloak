5498361b5eaa1fca8839c13a79c7c9ed661a6fec
Lucas Vilaboim <lucas@vilaboim.com>
2021-04-30 16:32:17 -0300

    feat(keycloak): create plugin

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import Keycloak from 'keycloak-js'
+
+function isFunction (value) {
+  return typeof value === 'function'
+}
+
+function init (config, options) {
+  const keycloak = Keycloak(config)
+
+  keycloak.onReady = function () {
+    isFunction(options.onReady) && options.onReady(keycloak)
+  }
+
+  keycloak.onAuthSuccess = function () {
+    isFunction(options.onAuthSuccess) && options.onAuthSuccess(keycloak)
+  }
+
+  keycloak.onAuthError = function () {
+    isFunction(options.onAuthError) && options.onAuthError(keycloak)
+  }
+
+  keycloak.onAuthRefreshSuccess = function () {
+    isFunction(options.onAuthRefreshSuccess) && options.onAuthRefreshSuccess(keycloak)
+  }
+
+  keycloak.onAuthRefreshError = function () {
+    isFunction(options.onAuthRefreshError) && options.onAuthRefreshError(keycloak)
+  }
+
+  keycloak
+    .init(options.init)
+    .then((authenticated) => {
+      isFunction(options.onInitSuccess) &&
+        options.onInitSuccess(authenticated)
+    })
+    .catch((err) => {
+      const error = Error('Could not initialized keycloak-js adapter')
+
+      isFunction(options.onInitError)
+        ? options.onInitError(error, err)
+        : console.error(error, err)
+    })
+
+  return keycloak
+}
+
+let installed = false
+
+const Vuecloak = {
+  install: (app, _options) => {
+    if (installed) return
+
+    installed = true
+
+    const defaultOptions = {
+      init: {
+        onLoad: 'login-required',
+        checkLoginIframe: false
+      }
+    }
+    const options = {
+      ...defaultOptions,
+      ..._options
+    }
+
+    const keycloak = init(options.config, options)
+
+    app.config.globalProperties.$keycloak = keycloak
+    app.provide('$keycloak', keycloak)
+  }
+}
+
+export { Vuecloak }
